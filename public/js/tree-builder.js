e1de5b55af7d78e9924b7fdddd404fa4547670d9
Sadrivilla <sadrisafwan@gmail.com>
2026-02-22 02:50:52 +0530

    Add tree-builder.js to build family tree structure

---
+++ public/js/tree-builder.js
@@ -0,0 +1,41 @@
+import { collection, getDocs } 
+from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+
+import { db } from "./firebase.js";
+
+async function buildTree() {
+
+  const snapshot = await getDocs(collection(db, "family_members"));
+  const members = [];
+
+  snapshot.forEach(doc => {
+    members.push({ id: doc.id, ...doc.data() });
+  });
+
+  const memberMap = {};
+  members.forEach(m => memberMap[m.id] = {
+      text: { name: m.name },
+      children: []
+  });
+
+  let rootNode = null;
+
+  members.forEach(m => {
+    if (m.fatherId) {
+      memberMap[m.fatherId].children.push(memberMap[m.id]);
+    } else {
+      rootNode = memberMap[m.id];
+    }
+  });
+
+  const chart_config = {
+    chart: {
+      container: "#tree"
+    },
+    nodeStructure: rootNode
+  };
+
+  new Treant(chart_config);
+}
+
+buildTree();
