206cd834fa07ff7a918eaabaf2642b482df871c2
Sadrivilla <sadrisafwan@gmail.com>
2026-02-22 02:58:02 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,9 +8,9 @@ import { getAuth } from
 "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyC7gkT4F_dRJpWfef12y6cwV3F2PTMJ6fY",
+  authDomain: "sadri-villa-14c06.firebaseapp.com",
+  projectId: "sadri-villa-14c06
 };
 
 const app = initializeApp(firebaseConfig);
