05a8130159dcac9345b03222ed05d3ec49854edb
Sadrivilla <sadrisafwan@gmail.com>
2026-02-24 04:19:22 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -7,13 +7,20 @@ import { getFirestore } from
 import { getAuth } from 
 "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 
+import { getStorage } from
+"https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyC7gkT4F_dRJpWfef12y6cwV3F2PTMJ6fY",
   authDomain: "sadri-villa-14c06.firebaseapp.com",
-  projectId: "sadri-villa-14c06"
+  projectId: "sadri-villa-14c06",
+  storageBucket: "sadri-villa-14c06.firebasestorage.app",
+  messagingSenderId: "687608762860",
+  appId: "1:687608762860:web:5ffdc557f3893d00e530c9"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const storage = getStorage(app);
