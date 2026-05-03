5760d869f00b23fcd3cde3eb93a8cc37536b8c6b
Sadrivilla <sadrisafwan@gmail.com>
2026-05-03 14:52:14 +0530

    Create analytics-tracker.js

---
+++ analytics-tracker.js
@@ -0,0 +1,41 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, collection, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const app = initializeApp({
+  apiKey:"AIzaSyC7gkT4F_dRJpWfef12y6cwV3F2PTMJ6fY",
+  authDomain:"sadri-villa-14c06.firebaseapp.com",
+  projectId:"sadri-villa-14c06"
+});
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+function getPageName(){
+  let page = window.location.pathname.split("/").pop().replace(".html","");
+  return page || "index";
+}
+
+async function trackPage(){
+  const page = getPageName();
+  const key = "visited_" + page;
+
+  if(sessionStorage.getItem(key)) return;
+
+  const user = auth.currentUser;
+  if(!user) return;
+
+  await setDoc(doc(collection(db,"analytics_logs")),{
+    page: page,
+    userId: user.uid,
+    timestamp: Date.now()
+  });
+
+  sessionStorage.setItem(key,"true");
+}
+
+onAuthStateChanged(auth, user=>{
+  if(user){
+    trackPage();
+  }
+});
