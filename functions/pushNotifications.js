2f3c2ac294bfc3834b6bd339f739e01c2d33cbe8
Sadrivilla <sadrisafwan@gmail.com>
2026-06-23 00:08:32 +0530

    Update pushNotifications.js

--- functions/pushNotifications.js
+++ functions/pushNotifications.js
@@ -3,9 +3,73 @@ const admin = require("firebase-admin");
 
 exports.sendPushNotification = functions.https.onCall(async (request) => {
 
-    return {
-        success: true,
-        message: "Push Notification Function Created"
-    };
+    try {
+
+        const token = request.data.token;
+        const title = request.data.title;
+        const body = request.data.body;
+        const url = request.data.url || "/dashboard.html";
+
+        if (!token) {
+
+            throw new functions.https.HttpsError(
+                "invalid-argument",
+                "FCM token is required."
+            );
+
+        }
+
+        const message = {
+
+            token: token,
+
+            notification: {
+
+                title: title || "Sadri Villa",
+
+                body: body || ""
+
+            },
+
+            data: {
+
+                url: url
+
+            },
+
+            webpush: {
+
+                notification: {
+
+                    icon: "/favicon.png",
+
+                    badge: "/favicon.png"
+
+                }
+
+            }
+
+        };
+
+        const response = await admin.messaging().send(message);
+
+        return {
+
+            success: true,
+
+            messageId: response
+
+        };
+
+    } catch (error) {
+
+        console.error(error);
+
+        throw new functions.https.HttpsError(
+            "internal",
+            error.message
+        );
+
+    }
 
 });
