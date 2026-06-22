ce042c1644b64f8b577dd08edddf73bd92a2d799
Sadrivilla <sadrisafwan@gmail.com>
2026-06-22 23:59:49 +0530

    Update pushNotifications.js

--- functions/pushNotifications.js
+++ functions/pushNotifications.js
@@ -1,1 +1,11 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
+exports.sendPushNotification = functions.https.onCall(async (request) => {
+
+    return {
+        success: true,
+        message: "Push Notification Function Created"
+    };
+
+});
